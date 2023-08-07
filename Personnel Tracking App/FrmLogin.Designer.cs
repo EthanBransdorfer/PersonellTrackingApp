1c190a565d174a02a61b60b7e2949f9e68618289
EthanBransdorfer <EthanJamesBransdorfer@gmail.com>
2023-08-07 00:00:14 -0400

    Created the main form. No styling yet.

--- Personnel Tracking App/FrmLogin.Designer.cs
+++ Personnel Tracking App/FrmLogin.Designer.cs
@@ -103,7 +103,7 @@
             this.Controls.Add(this.txtUserNo);
             this.Controls.Add(this.UserNo);
             this.Name = "FrmLogin";
-            this.Text = "Form1";
+            this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
 
---
+++ Personnel Tracking App/FrmMain.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Personnel_Tracking_App
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEmployee = new System.Windows.Forms.Button();
+            this.btnTask = new System.Windows.Forms.Button();
+            this.btnSalary = new System.Windows.Forms.Button();
+            this.btnPermission = new System.Windows.Forms.Button();
+            this.btnDepartment = new System.Windows.Forms.Button();
+            this.btnPosition = new System.Windows.Forms.Button();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEmployee
+            // 
+            this.btnEmployee.Location = new System.Drawing.Point(12, 12);
+            this.btnEmployee.Name = "btnEmployee";
+            this.btnEmployee.Size = new System.Drawing.Size(110, 81);
+            this.btnEmployee.TabIndex = 0;
+            this.btnEmployee.Text = "Employee";
+            this.btnEmployee.UseVisualStyleBackColor = true;
+            this.btnEmployee.Click += new System.EventHandler(this.btnEmployee_Click);
+            // 
+            // btnTask
+            // 
+            this.btnTask.Location = new System.Drawing.Point(128, 12);
+            this.btnTask.Name = "btnTask";
+            this.btnTask.Size = new System.Drawing.Size(110, 81);
+            this.btnTask.TabIndex = 1;
+            this.btnTask.Text = "Task";
+            this.btnTask.UseVisualStyleBackColor = true;
+            // 
+            // btnSalary
+            // 
+            this.btnSalary.Location = new System.Drawing.Point(244, 12);
+            this.btnSalary.Name = "btnSalary";
+            this.btnSalary.Size = new System.Drawing.Size(110, 81);
+            this.btnSalary.TabIndex = 2;
+            this.btnSalary.Text = "Salary";
+            this.btnSalary.UseVisualStyleBackColor = true;
+            // 
+            // btnPermission
+            // 
+            this.btnPermission.Location = new System.Drawing.Point(12, 111);
+            this.btnPermission.Name = "btnPermission";
+            this.btnPermission.Size = new System.Drawing.Size(110, 81);
+            this.btnPermission.TabIndex = 3;
+            this.btnPermission.Text = "Permission";
+            this.btnPermission.UseVisualStyleBackColor = true;
+            // 
+            // btnDepartment
+            // 
+            this.btnDepartment.Location = new System.Drawing.Point(128, 111);
+            this.btnDepartment.Name = "btnDepartment";
+            this.btnDepartment.Size = new System.Drawing.Size(110, 81);
+            this.btnDepartment.TabIndex = 4;
+            this.btnDepartment.Text = "Department";
+            this.btnDepartment.UseVisualStyleBackColor = true;
+            // 
+            // btnPosition
+            // 
+            this.btnPosition.Location = new System.Drawing.Point(244, 111);
+            this.btnPosition.Name = "btnPosition";
+            this.btnPosition.Size = new System.Drawing.Size(110, 81);
+            this.btnPosition.TabIndex = 5;
+            this.btnPosition.Text = "Position";
+            this.btnPosition.UseVisualStyleBackColor = true;
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.Location = new System.Drawing.Point(12, 216);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(110, 81);
+            this.btnLogOut.TabIndex = 6;
+            this.btnLogOut.Text = "LogOut";
+            this.btnLogOut.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(244, 216);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(110, 81);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 327);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogOut);
+            this.Controls.Add(this.btnPosition);
+            this.Controls.Add(this.btnDepartment);
+            this.Controls.Add(this.btnPermission);
+            this.Controls.Add(this.btnSalary);
+            this.Controls.Add(this.btnTask);
+            this.Controls.Add(this.btnEmployee);
+            this.Name = "FrmMain";
+            this.Text = "Employee Tracking";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEmployee;
+        private System.Windows.Forms.Button btnTask;
+        private System.Windows.Forms.Button btnSalary;
+        private System.Windows.Forms.Button btnPermission;
+        private System.Windows.Forms.Button btnDepartment;
+        private System.Windows.Forms.Button btnPosition;
+        private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
